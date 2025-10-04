cf10e6b78a61a3c676650c2cf76ab8a9b6d86166
Alireza Sharifpour <ascode94@gmail.com>
2025-10-04 11:33:08 +0330

    Remove TestPage component and associated route from the application
    - Deleted the TestPage component from the pages directory to streamline the codebase.
    - Removed the corresponding route from the AppRoutes to eliminate unused navigation paths.
    - Updated the routes configuration to reflect the removal of the TestPage route.

--- src/app/router/routes.tsx
+++ src/app/router/routes.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { HomePage } from "@/pages/HomePage";
-import { TestPage } from "@/pages/TestPage";
+
 import NewCampaignPage from "@/pages/NewCampaignPage";
 import { CampaignPage } from "@/pages/CampaignPage";
 import { ROUTES } from "@/shared/config/routes";
@@ -9,7 +9,6 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.TEST} element={<TestPage />} />
       <Route path={ROUTES.CAMPAIGNS_NEW} element={<NewCampaignPage />} />
       <Route path={ROUTES.CAMPAIGNS_DETAIL} element={<CampaignPage />} />
     </Routes>
