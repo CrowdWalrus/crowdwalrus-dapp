4d09e6ea4ada0d97c9254d5adb3c0d9be0b1bd4c
Alireza Sharifpour <ascode94@gmail.com>
2025-12-20 11:08:51 +0330

    feat: Export CampaignVerificationModal and related types from admin components index

--- src/features/admin/components/index.ts
+++ src/features/admin/components/index.ts
@@ -1,2 +1,6 @@
 export { CampaignCard } from "./CampaignCard";
+export {
+  CampaignVerificationModal,
+  type CampaignVerificationAction,
+} from "./CampaignVerificationModal";
 export { VerifierManagementPanel } from "./VerifierManagementPanel";
